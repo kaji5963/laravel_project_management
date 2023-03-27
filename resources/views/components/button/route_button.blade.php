8fec7f1b57a135e20ae5f0b204cd0c46c4ad497f
laravel_admin <fd.elt.119@gmail.com>
2023-03-27 15:36:56 +0900

    navigation修正

--- resources/views/components/button/route_button.blade.php
+++ resources/views/components/button/route_button.blade.php
@@ -5,5 +5,5 @@
 ])
 
 <button
-  class="ml-auto text-white bg-{{ $color }}-500 border-0 py-2 px-6 focus:outline-none hover:bg-{{ $color }}-600 rounded"><a
+  class="ml-auto text-white bg-{{ $color }}-500 border-0 py-2 px-4 focus:outline-none hover:bg-{{ $color }}-600 rounded"><a
     href="{{ route($pass, $params) }}">{{ $slot }}</a></button>
